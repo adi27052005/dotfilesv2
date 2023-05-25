664901f2d49e2adda66a81aaf7af165401b02a69
adi27052005 <132362628+adi27052005@users.noreply.github.com>
2023-05-25 22:35:41 +0530

    Add files via upload

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-case "$(printf 'Shutdown\nReboot\nLock\nLogout' | rofi -dmenu -p 'Choose')" in
+case "$(printf '󰐥 Shutdown\n Reboot\n Lock\n󱅟 Logout' | rofi -dmenu -p 'Choose 󰧂')" in
 
-    'Shutdown') sudo poweroff ;;
-    'Reboot') sudo reboot ;;
-    'Lock') slock ;;
-    'Logout') i3-msg exit ;;
+    '󰐥 Shutdown') sudo poweroff ;;
+    ' Reboot') sudo reboot ;;
+    ' Lock') slock ;;
+    '󱅟 Logout') i3-msg exit ;;
     *) exit 1 ;;
 esac
